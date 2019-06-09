9d606437452502fae4d5967144216eb9da55b375
tanishka99 <51483306+tanishka99@users.noreply.github.com>
2019-06-09 07:13:26 +0000

    Update dir.py

--- dir.py
+++ dir.py
@@ -1,9 +1,6 @@
 #!usr/bin/python3
 import os
-#root_path="/home/adhoc/Desktop"
-#folders=['fol1','fol2','fol3']
-#for folder in folders:
-#	os.mkdir(os.path.join(root_path,folder
+
 for i in range(500):
 	dirname=str(i)
 	os.mkdir(dirname)
