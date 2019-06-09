93f3064dcd44bc3b01552aaa9de696b08083ccf0
tanishka99 <51483306+tanishka99@users.noreply.github.com>
2019-06-09 07:11:52 +0000

    Add files via upload
    how to make multiple directories through python?

---
+++ dir.py
@@ -0,0 +1,11 @@
+#!usr/bin/python3
+import os
+#root_path="/home/adhoc/Desktop"
+#folders=['fol1','fol2','fol3']
+#for folder in folders:
+#	os.mkdir(os.path.join(root_path,folder
+for i in range(500):
+	dirname=str(i)
+	os.mkdir(dirname)
+	print("directry created",i )
+
