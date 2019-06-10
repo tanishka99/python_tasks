b29559025c1bc56ac21c4e876e6dffc69ebedd6f
tanishka99 <51483306+tanishka99@users.noreply.github.com>
2019-06-10 19:02:26 +0000

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,3 +1,8 @@
+Problem 1 :
+
+ Create a program that asks the user to enter their name and their age.
+ Print out a message that will tell them the year that they will turn 95 years old.
+
 #!usr/bin/python3
 n=input("What is your name")
 a=int(input("what is your age?"))
