7f159402d2f7d535ed5fcd83ad195d1a30361d36
tanishka99 <51483306+tanishka99@users.noreply.github.com>
2019-06-10 18:06:25 +0000

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -1,3 +1,10 @@
+#Problem 2 : 
+
+#write a code using  that will take user input from and search on google and store top 10 url in the list.
+#conditions : 
+ #   i )   URL must be stored permanently as well
+#    ii)   user can give input using keyboard and  voice both
+
 #!usr/bin/python3
 
 import time
