543f9db35bdf4634cb5186eaad96d5caf412d4d1
tanishka99 <51483306+tanishka99@users.noreply.github.com>
2019-06-10 19:01:37 +0000

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,8 @@
+#!usr/bin/python3
+n=input("What is your name")
+a=int(input("what is your age?"))
+year=int(input("what is the current year?"))
+diff=year-a
+final=diff+95
+print("year in which u will be 95 years old is :")
+print(final)
