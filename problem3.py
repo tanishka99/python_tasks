65007b4aa0be8e22db95ebd0e598aa67fb0860ec
tanishka99 <51483306+tanishka99@users.noreply.github.com>
2019-06-11 19:01:02 +0530

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -12,3 +12,18 @@ x=[i for i in adhoc if i>5]
 print(x)
 y=[i for i in adhoc if i<=2]
 print(y)
+
+or 
+
+adhoc=[1,2,3,1,4,5,66,22,2,6,0,9]
+less=[]
+greater=[]
+for i in adhoc:
+    if i<=2:
+        less.append(i)
+    elif i>5:
+        greater.append(i)
+print("values less than or equal to 2:\n")
+print(less)
+print("values greater than 5:")
+print(greater)
