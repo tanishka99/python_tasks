4dee62aee880ed6c7087c4a8778819d3f2c5df2e
tanishka99 <51483306+tanishka99@users.noreply.github.com>
2019-06-13 17:29:14 +0000

    Add files via upload

---
+++ recv.py
@@ -0,0 +1,22 @@
+#!usr/bin/python2
+
+import socket
+recv_ip="127.0.0.1"
+recv_port=4444 # 0-1024 -- you can chck free udp port netstat -nulp
+
+#creating udp socket
+# ip type V4,UDP
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+#fitting ip and port with udp socket
+s.bind((recv_ip,recv_port))
+
+#receiving data from sender
+while 4 > 2:
+	data=s.recvfrom(100)
+	print("messgae from sender",data[0])
+	print("Sender ip +port --socket",data[1])
+#reply to sender
+
+	rply=raw_input("type your rply:")
+	s.sendto("rply",data[1])
+s.close()
